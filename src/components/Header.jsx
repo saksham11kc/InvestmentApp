08bcf43eae48bc583e14129e00d01d3504c3938a
Saksham KC <sakshamkc@Sakshams-MacBook-Air.local>
2024-09-11 14:39:25 +0545

    Create Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "../index.css";
+
+import headerImage from "../assets/investment-calculator-logo.png";
+export default function Header() {
+  return (
+    <header id="header">
+      <img src={headerImage} alt="Pile of money logo" />
+      <h1>React Investment Calculator</h1>
+    </header>
+  );
+}
